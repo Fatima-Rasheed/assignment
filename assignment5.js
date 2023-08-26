e595c75e2894b6a4aedffddbc32affa16cf302c2
Fatima-Rasheed <fatimarasheedch@gmail.com>
2023-08-26 05:47:45 +0500

    assignment5

---
+++ assignment5.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question no 1
+var sum = 0;
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+        sum += i;
+    }
+}
+console.log(sum);
---
+++ assignment5.ts
@@ -0,0 +1,7 @@
+// Question no 1
+var sum:number=0;
+for(let i=0;i<=20;i++){
+    if(i%2==0){
+        sum+=i;
+    }
+}console.log(sum);
