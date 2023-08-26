351f5a680e727d54e855bfb27e5ecb712c7b0a01
Fatima-Rasheed <fatimarasheedch@gmail.com>
2023-08-26 06:00:40 +0500

    assignment5

--- assignment5.js
+++ assignment5.js
@@ -7,3 +7,12 @@ for (let i = 0; i <= 20; i++) {
     }
 }
 console.log(sum);
+// Question no 2
+var array = [20, 56, 77, 89, 44];
+var evenNumbers = [];
+for (let j = 1; j <= array.length; j++) {
+    if (j % 2 == 0) {
+        evenNumbers.push(j);
+    }
+}
+console.log(evenNumbers);
--- assignment5.ts
+++ assignment5.ts
@@ -5,3 +5,12 @@ for(let i=0;i<=20;i++){
         sum+=i;
     }
 }console.log(sum);
+// Question no 2
+var array:number[]=[20,56,77,89,44];
+var evenNumbers:number[]=[]
+for(let j:number=1;j<=array.length;j++){
+    if(j%2==0){
+        evenNumbers.push(j)
+    }
+
+}console.log(evenNumbers)
